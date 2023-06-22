74dc8494ef6e04b54909d81e68f5055c05f5a4c5
Martin Yeo <40734014+trexfeathers@users.noreply.github.com>
2023-06-22 11:01:10 +0100

    Starter Peloton setup. (#1)

---
+++ peloton/common.py
@@ -0,0 +1,36 @@
+"""
+This module visualises SciTools issues and PR's in a dashboard, to assist the
+dev team in keeping the repos healthy.
+"""
+
+# TODO: use the SciTools/peloton user group instead.
+peloton_logins = [
+    "bjlittle",
+    "bsherratt",
+    "ESadek-MO",
+    "codecov[bot]",
+    "HGWright",
+    "jamesp",
+    "lbdreyer",
+    "pp-mo",
+    "rcomer",
+    "stephenworsley",
+    "tkknight",
+    "trexfeathers",
+    "wjbenfold",
+    "dependabot[bot]",
+    "github-actions[bot]",
+    "pre-commit-ci[bot]",
+    "scitools-ci[bot]",
+]
+
+# TODO: expand list if this idea gets traction. Possibly blanket all SciTools?
+peloton_repos = [
+    "SciTools/cf-units",
+    "SciTools/iris",
+    "SciTools/iris-grib",
+    "SciTools/nc-time-axis",
+    "SciTools/python-stratify",
+    "SciTools/tephi",
+    "SciTools-incubator/iris-esmf-regrid",
+]
