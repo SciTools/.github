ba3089d8a1d77aff12ac7c236ce0c92b1bbe8815
Martin Yeo <40734014+trexfeathers@users.noreply.github.com>
2023-12-14 18:44:28 +0000

    Use curly braces for parameter expansion.
    Co-authored-by: Bill Little <bill.little@metoffice.gov.uk>

--- peloton/update-schema.sh
+++ peloton/update-schema.sh
@@ -3,7 +3,7 @@
 # https://github.com/profusion/sgqlc/blob/fbee55d92554dd209b4e3ebda093f371bbc12aca/examples/github/update-schema.sh
 # https://sgqlc.readthedocs.io/en/latest/sgqlc.codegen.html#generating-schema-types
 
-if [ "$NO_DOWNLOAD" != 1 ]; then
+if [ "${NO_DOWNLOAD}" != 1 ]; then
     if [ -z "$GH_TOKEN" ]; then
         echo "please export GH_TOKEN with your GitHub API token" >&2
         echo "see: https://github.com/settings/tokens" >&2
