7b320886c66fd157c386fd2528da519e6fec247e
Martin Yeo <martin.yeo@metoffice.gov.uk>
2025-03-18 14:47:58 +0000

    Move _templating_scripting shebang to the very top.

--- templates/_templating_scripting.py
+++ templates/_templating_scripting.py
@@ -1,7 +1,6 @@
+#!/usr/bin/env python3
 """Commands and scripts for administrating templating of files across SciTools repos.
 """
-#!/usr/bin/env python3
-
 import argparse
 import json
 from pathlib import Path
